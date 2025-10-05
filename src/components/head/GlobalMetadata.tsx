ce58a2c3178fe483f325357276ba9abb18e0fcb8
sh1n3sh1n3 <sh1n3sh1n3.03@gmail.com>
2025-10-06 01:11:52 +0800

    chore: updated favicon

--- src/components/head/GlobalMetadata.tsx
+++ src/components/head/GlobalMetadata.tsx
@@ -32,7 +32,9 @@ export default function GlobalMetadata() {
 
       {/* Favicon and app icons */}
       <link rel="icon" href="/favicon/favicon.ico" />
-      <link rel="apple-touch-icon" href="/favicon/apple-touch-icon.svg" />
+      <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
+      <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
+      <link rel="apple-touch-icon" href="/favicon/apple-touch-icon.png" />
       <link rel="manifest" href="/manifest.json" />
     </Head>
   );
